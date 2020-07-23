f253b4ff5dcbe483837c7f2b2f6ebcf9648a16bf
Anwesh43 <anweshthecool0@gmail.com>
2020-07-23 13:28:29 +0530

    adding ScaleUtil class

--- index.ts
+++ index.ts
@@ -7,6 +7,21 @@ const sizeFactor : number = 4
 const foreColor : string = "indigo"
 const backColor : string = "#bdbdbd"
 
+class ScaleUtil {
+
+    static maxScale(scale : number, i : number, n : number) : number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.divideScale(scale, i, n)) * n
+    }
+
+    static sinify(scale : number) : number {
+        return Math.sin(scale * Math.PI)
+    }
+}
+
 class Stage {
 
     canvas : HTMLCanvasElement = document.createElement('canvas')
